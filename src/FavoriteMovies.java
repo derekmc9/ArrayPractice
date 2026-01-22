1c7aaa62249de15abb36f89b2a85ba7e7af24647
derekmc9 <33237943+derekmc9@users.noreply.github.com>
2026-01-22 12:29:38 -0500

    Array practice

---
+++ src/FavoriteMovies.java
@@ -0,0 +1,21 @@
+// Challenge 1: Favorite Movies
+// File: FavoriteMovies.java
+// Instructions: Create an array of your 5 favorite movies and print them with numbers
+
+public class FavoriteMovies {
+    public static void main(String[] args) {
+        // TODO: Create a String array called 'movies' with 5 of your favorite movies
+
+
+        // TODO: Print a header message
+        System.out.println("My Favorite Movies:");
+        System.out.println("==================");
+
+        // TODO: Use a for loop to print each movie with its number (1-5)
+        // Example output:
+        // 1. The Shawshank Redemption
+        // 2. Inception
+        // etc.
+
+    }
+}
