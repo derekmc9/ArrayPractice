1856baee83fd96d295f14546f2e50ca69027da4c
derekmc9 <33237943+derekmc9@users.noreply.github.com>
2026-01-22 13:09:24 -0500

    Array practice

---
+++ src/ReverseArray.java
@@ -0,0 +1,26 @@
+// Challenge 3: Reverse Array
+// File: ReverseArray.java
+// Instructions: Reverse the order of elements in an array
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        // Given array
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // TODO: Print the original array
+        System.out.println("Original array:");
+
+
+        // TODO: Create a new array to store the reversed elements
+
+
+        // TODO: Use a for loop to copy elements in reverse order
+        // Hint: reversed[0] should equal numbers[4]
+        //       reversed[1] should equal numbers[3], etc.
+
+
+        // TODO: Print the reversed array
+        System.out.println("\nReversed array:");
+
+    }
+}
---
+++ src/TestScoreAverage.java
@@ -0,0 +1,26 @@
+// Challenge 2: Test Score Average
+// File: TestScoreAverage.java
+// Instructions: Calculate and display the average of test scores
+
+public class TestScoreAverage {
+    public static void main(String[] args) {
+        // TODO: Create an int array called 'scores' with at least 5 test scores
+        // Example: {85, 92, 78, 95, 88}
+
+
+        // TODO: Create a variable to store the sum of all scores
+
+
+        // TODO: Use a for loop to add up all the scores
+
+
+        // TODO: Calculate the average (sum divided by number of scores)
+
+
+        // TODO: Print the results
+        // Example output:
+        // Test Scores: 85, 92, 78, 95, 88
+        // Average Score: 87.6
+
+    }
+}
